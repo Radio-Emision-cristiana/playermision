65d0c3bafef22e82446af96d466736222b38fc2d
Radio-Emision-cristiana <radioemisioncristiana@gmail.com>
2026-06-30 22:14:17 -0400

    Refactor openInstagram function for better handling

--- main_sin_visualizador.js
+++ main_sin_visualizador.js
@@ -902,17 +902,20 @@ function openFacebook() {
 }
 
 // Función para Instagram
-unction openInstagram() {
-    const appUrl = "instagram://user?username=radioemisioncristiana";
-    const webUrl = "https://www.instagram.com/radioemisioncristiana/";
-
-    window.location.href = appUrl;
-
-    setTimeout(() => {
-        window.location.href = webUrl;
-    }, 1500);
+function openInstagram() {
+    console.log('📱 Abriendo Instagram...');
+    if (isMobile()) {
+        // Intenta abrir la app de Instagram primero
+        window.location.href = 'instagram://user?username=radioemisioncristiana.gran';
+        // Fallback a la web después de un timeout
+        setTimeout(() => {
+            window.open('https://www.instagram.com/radioemisioncristiana', '_blank');
+        }, 1000);
+    } else {
+        // En desktop, abre la web directamente
+        window.open('https://www.instagram.com/radioemisioncristiana', '_blank');
+    }
 }
-
 // Función para Twitter
 function openTwitter() {
     console.log('📱 Abriendo Twitter...');
